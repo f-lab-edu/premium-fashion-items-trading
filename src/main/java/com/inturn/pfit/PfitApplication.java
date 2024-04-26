f0565d1ac77b4f47280bcd9c9343605d070a02ec
inturn86 <cjswoiie@naver.com>
2024-04-26 22:18:30 +0900

    init : project init

---
+++ src/main/java/com/inturn/pfit/PfitApplication.java
@@ -0,0 +1,13 @@
+package com.inturn.pfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfitApplication.class, args);
+	}
+
+}

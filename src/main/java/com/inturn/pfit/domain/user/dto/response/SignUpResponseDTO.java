8ac92ff83464e5ac6744828a518747e6664c78a5
inturn86 <cjswoiie@naver.com>
2024-05-03 01:11:09 +0900

    refactor : 사용하지 않는 import 제거

--- src/main/java/com/inturn/pfit/domain/user/dto/response/SignUpResponseDTO.java
+++ src/main/java/com/inturn/pfit/domain/user/dto/response/SignUpResponseDTO.java
@@ -1,8 +1,10 @@
 package com.inturn.pfit.domain.user.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class SignUpResponseDTO {
 
 	private Long userId;
--- src/main/java/com/inturn/pfit/domain/user/repository/UserRepository.java
+++ src/main/java/com/inturn/pfit/domain/user/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.inturn.pfit.domain.user.repository;
 
-import com.inturn.pfit.domain.user.entity.User;
 import com.inturn.pfit.domain.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;

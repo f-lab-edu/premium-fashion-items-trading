e3f68cabca50133342a036fd2df34ae5233ab424
inturn86 <cjswoiie@naver.com>
2024-05-07 12:59:18 +0900

    feat : 사용하지 않는 메소드 제거 feat : QueryDsl ResponseDTO class -> record로 전환 feat : QueryDsl Projections.bean -> constructor 로 변경

--- src/main/java/com/inturn/pfit/domain/category/dto/response/CategoryPagingResponseDTO.java
+++ src/main/java/com/inturn/pfit/domain/category/dto/response/CategoryPagingResponseDTO.java
@@ -1,20 +1,12 @@
 package com.inturn.pfit.domain.category.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Builder;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class CategoryPagingResponseDTO {
-
-
-	Integer categoryId;
-	String categoryName;
-	Integer categorySort;
-
+@Builder
+public record CategoryPagingResponseDTO (
+		Integer categoryId,
+		String categoryName,
+		Integer categorySort
+) {
 
 }

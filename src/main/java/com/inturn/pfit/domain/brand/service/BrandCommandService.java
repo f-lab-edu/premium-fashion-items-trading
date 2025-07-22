df3cc41ce457da2e4293c640f2651d70ee5e6b7e
inturn86 <cjswoiie@naver.com>
2025-07-22 23:54:44 +0900

    chore : test 1

--- src/main/java/com/inturn/pfit/domain/brand/service/BrandCommandService.java
+++ src/main/java/com/inturn/pfit/domain/brand/service/BrandCommandService.java
@@ -25,7 +25,6 @@ public class BrandCommandService {
 	public CreateBrandResponseDTO createBrand(CreateBrandRequestDTO req) {
 
 		Brand brand = req.createBrand();
-		System.out.printf("s");
 
 		return CreateBrandResponseDTO.from(this.save(brand));
 	}

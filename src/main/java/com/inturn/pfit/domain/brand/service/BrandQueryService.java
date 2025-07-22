78820bf5cbe5061d008db33e82f6ee42b18a59ad
inturn86 <cjswoiie@naver.com>
2025-07-22 23:45:27 +0900

    chore : test remove

--- src/main/java/com/inturn/pfit/domain/brand/service/BrandQueryService.java
+++ src/main/java/com/inturn/pfit/domain/brand/service/BrandQueryService.java
@@ -21,14 +21,7 @@ public class BrandQueryService {
 
 	@Transactional(readOnly = true)
 	public Page<BrandPagingResponseDTO> getBrandPagingList(BrandPagingRequestDTO req, Pageable page) {
-		System.out.printf("main");
-		System.out.printf("main2");
-		System.out.printf("mainmain");
-		System.out.printf("main3");
-		System.out.printf("main3");
-		System.out.printf("main3");
-		System.out.printf("main3");
-		System.out.printf("main3");
+
 		return brandRepository.getPagingList(req, page);
 	}
 

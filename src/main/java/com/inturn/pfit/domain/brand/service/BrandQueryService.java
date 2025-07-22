9eb78472865a81d5e57b40775e8b003c87214469
inturn86 <cjswoiie@naver.com>
2025-07-22 23:43:31 +0900

    chore : test main 3

--- src/main/java/com/inturn/pfit/domain/brand/service/BrandQueryService.java
+++ src/main/java/com/inturn/pfit/domain/brand/service/BrandQueryService.java
@@ -24,6 +24,8 @@ public class BrandQueryService {
 		System.out.printf("main");
 		System.out.printf("main2");
 		System.out.printf("main3");
+		System.out.printf("main3");
+		System.out.printf("main3");
 		return brandRepository.getPagingList(req, page);
 	}
 
